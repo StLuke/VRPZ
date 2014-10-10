507279395b3dd1cffafc35a60734b4b080ee5df4
Branislav Blaskovic <bblaskov@redhat.com>
2014-10-10 17:55:08 +0200

    Code cleanup

--- stuff/lol.py
+++ stuff/lol.py
@@ -5,13 +5,6 @@ import numpy as np #Imports NumPy
 import cv,cv2 #Uses both of cv and cv2
 import pygame #Uses pygame
 
-#The libaries below are used for mouse manipulation
-from Xlib import X, display
-import Xlib.XK
-import Xlib.error
-import Xlib.ext.xtest
-import socket
-
 constList = lambda length, val: [val for _ in range(length)] #Gives a list of size length filled with the variable val. length is a list and val is dynamic
 
 """
@@ -57,47 +50,6 @@ class BlobAnalysis:
         self.cHullArea = cHullArea
         self.contourArea = contourArea
 
-d = display.Display() #Display reference for Xlib manipulation
-def move_mouse(x,y):#Moves the mouse to (x,y). x and y are ints
-    return
-    s = d.screen()
-    root = s.root
-    root.warp_pointer(x,y)
-    d.sync()
-
-def click_down(button):#Simulates a down click. Button is an int
-    return
-    Xlib.ext.xtest.fake_input(d,X.ButtonPress, button)
-    d.sync()
-
-def click_up(button): #Simulates a up click. Button is an int
-    return
-    Xlib.ext.xtest.fake_input(d,X.ButtonRelease, button)
-    d.sync()
-
-"""
-The function below is a basic mean filter. It appends a cache list and takes the mean of it.
-It is useful for filtering noisy data
-cache is a list of floats or ints and val is either a float or an int
-it returns the filtered mean
-"""
-def cacheAppendMean(cache, val):
-    cache.append(val)
-    del cache[0]
-    return np.mean(cache)
-
-UDP_IP = "0.0.0.0"
-UDP_PORT = 5005
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
-sock.setblocking(0)
-
-#while True:
-#    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-#    print "received message:", data
-
 
 """
 This is the GUI that displays the thresholded image with the convex hull and centroids. It uses pygame.
@@ -105,8 +57,6 @@ Mouse control is also dictated in this function because the mouse commands are u
 """
 def hand_tracker():
     (depth,_) = get_depth()
-    cHullAreaCache = constList(5,12000) #Blank cache list for convex hull area
-    areaRatioCache = constList(5,1) #Blank cache list for the area ratio of contour area to convex hull area
     centroidList = list() #Initiate centroid list
     #RGB Color tuples
     BLACK = (0,0,0)
@@ -123,7 +73,6 @@ def hand_tracker():
     screenFlipped = pygame.display.set_mode((xSize,ySize),pygame.RESIZABLE) #creates surface that will be flipped (mirror display)
     screen.fill(BLACK) #Make the window black
     done = False #Iterator boolean --> Tells programw when to terminate
-    dummy = False #Very important bool for mouse manipulation
 
 
 
@@ -148,6 +97,7 @@ def hand_tracker():
 
         for cont in blobDataBack.contours: #Iterates through contours in the background
             pygame.draw.lines(screen,YELLOW,True,cont,3) #Colors the binary boundaries of the background yellow
+
             for xcont,ycont in cont:
                 if ycont < maxCont[1]:
                     maxCont = (xcont, ycont)
@@ -155,26 +105,20 @@ def hand_tracker():
         xcord = maxCont[0] - (imgCow.get_rect().size[0]/2)
         ycord = maxCont[1] - (imgCow.get_rect().size[1]/2)
 
+        # Hlava?
         screen.blit(imgCow, (xcord, ycord+100))
-
-
-        """ SVG
-        data = array.array('c', chr(0) * WIDTH * HEIGHT * 4)
-        surface = cairo.ImageSurface.create_for_data(
-            data, cairo.FORMAT_ARGB32, WIDTH, HEIGHT, WIDTH * 4)
-        """
-
-
 
         for i in range(blobData.counter): #Iterate from 0 to the number of blobs minus 1
 
             pygame.draw.circle(screen,BLUE,blobData.centroid[i],10) #Draws a blue circle at each centroid
 
             centroidList.append(blobData.centroid[i]) #Adds the centroid tuple to the centroidList --> used for drawing
-            #pygame.draw.lines(screen,RED,True,blobData.cHull[i],3) #Draws the convex hull for each blob
-            #pygame.draw.lines(screen,GREEN,True,blobData.contours[i],3) #Draws the contour of each blob
+            pygame.draw.lines(screen,RED,True,blobData.cHull[i],3) #Draws the convex hull for each blob
+            pygame.draw.lines(screen,GREEN,True,blobData.contours[i],3) #Draws the contour of each blob
             mostLeft = (xSize, 0)
             mostRight = (0, 0)
+
+            # Body ruky
             for tips in blobData.cHull[i]: #Iterates through the verticies of the convex hull for each blob
                 #pygame.draw.circle(screen,PURPLE,tips,5) #Draws the vertices purple
 
@@ -183,62 +127,14 @@ def hand_tracker():
                 if tips[0] > mostRight[0]:
                     mostRight = tips
 
+            # Centrum ruky
             pygame.draw.circle(screen,WHITE,blobData.centroid[i],30)
-
-            #print blobData.centroid[i]
-
-
-
-            """ SVG stuff
-            svg = rsvg.Handle(file="../graphics/cow.svg")
-            ctx = cairo.Context(surface)
-            svg.render_cairo(ctx)
-            image = pygame.image.frombuffer(data.tostring(), (WIDTH, HEIGHT),"ARGB")
-            screen.blit(image, blobData.centroid[i])
-            """
-
-
-        """
-        #Drawing Loop
-        #This draws on the screen lines from the centroids
-        #Possible exploration into gesture recognition :D
-        for cent in centroidList:
-            pygame.draw.circle(screen,BLUE,cent,10)
-        """
 
         pygame.display.set_caption('ZOO') #Makes the caption of the pygame screen 'Kinect Tracking'
         del depth #Deletes depth --> opencv memory issue
         screenFlipped = pygame.transform.flip(screen,1,0) #Flips the screen so that it is a mirror display
         screen.blit(screenFlipped,(0,0)) #Updates the main screen --> screen
         pygame.display.flip() #Updates everything on the window
-
-        #Mouse Try statement
-        try:
-            centroidX = blobData.centroid[0][0]
-            centroidY = blobData.centroid[0][1]
-            if dummy:
-                mousePtr = display.Display().screen().root.query_pointer()._data #Gets current mouse attributes
-                dX = centroidX - strX #Finds the change in X
-                dY = strY - centroidY #Finds the change in Y
-                if abs(dX) > 1: #If there was a change in X greater than 1...
-                    mouseX = mousePtr["root_x"] - 2*dX #New X coordinate of mouse
-                if abs(dY) > 1: #If there was a change in Y greater than 1...
-                    mouseY = mousePtr["root_y"] - 2*dY #New Y coordinate of mouse
-                move_mouse(mouseX,mouseY) #Moves mouse to new location
-                strX = centroidX #Makes the new starting X of mouse to current X of newest centroid
-                strY = centroidY #Makes the new starting Y of mouse to current Y of newest centroid
-                cArea = cacheAppendMean(cHullAreaCache,blobData.cHullArea[0]) #Normalizes (gets rid of noise) in the convex hull area
-                areaRatio = cacheAppendMean(areaRatioCache, blobData.contourArea[0]/cArea) #Normalizes the ratio between the contour area and convex hull area
-                if cArea < 10000 and areaRatio > 0.82: #Defines what a click down is. Area must be small and the hand must look like a binary circle (nearly)
-                    click_down(1)
-                else:
-                    click_up(1)
-            else:
-                strX = centroidX #Initializes the starting X
-                strY = centroidY #Initializes the starting Y
-                dummy = True #Lets the function continue to the first part of the if statement
-        except: #There may be no centroids and therefore blobData.centroid[0] will be out of range
-            dummy = False #Waits for a new starting point
 
         for e in pygame.event.get(): #Itertates through current events
             if e.type is pygame.QUIT: #If the close button is pressed, the while loop ends
