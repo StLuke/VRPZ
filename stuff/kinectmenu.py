2ead70aa63a1a6cdc6a17e5f3c6b5aeb1b0e2296
Frantisek Sumsal <frantisek.sumsal@gmail.com>
2014-10-11 11:57:15 +0200

    spring cleaning #3

--- stuff/kinectmenu.py
+++ stuff/kinectmenu.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-from freenect import sync_get_depth as get_depth #Uses freenect to get depth information from the Kinect
-import numpy as np #Imports NumPy
-import cv,cv2 #Uses both of cv and cv2
-import pygame #Uses pygame
+from freenect import sync_get_depth as get_depth 
+import numpy as np
+import cv,cv2
+import pygame
 import sys
 import time
 import random
@@ -15,7 +15,8 @@ import Xlib.XK
 import Xlib.error
 import Xlib.ext.xtest
 
-constList = lambda length, val: [val for _ in range(length)] #Gives a list of size length filled with the variable val. length is a list and val is dynamic
+# Gives a list of size length filled with the variable val. length is a list and val is dynamic
+constList = lambda length, val: [val for _ in range(length)] 
 
 # Class for bouncing animal image
 class BouncingSprite(pygame.sprite.Sprite):
@@ -119,24 +120,35 @@ class IdleScreen():
 	def run(self):
 		screenloop = True
 		(depth,_) = get_depth()
-		cHullAreaCache = constList(5,12000) #Blank cache list for convex hull area
-		areaRatioCache = constList(5,1) #Blank cache list for the area ratio of contour area to convex hull area
-		centroidList = list() #Initiate centroid list
+		# Blank cache list for convex hull area
+		cHullAreaCache = constList(5,12000)
+		# Blank cache list for the area ratio of contour area to convex hull area
+		areaRatioCache = constList(5,1)
+		# Initiate centroid list
+		centroidList = list()
 		screenFlipped = pygame.display.set_mode((self.scrWidth, self.scrHeight), 0, 32)
-		done = False #Iterator boolean --> Tells programw when to terminate
-		dummy = False #Very important bool for mouse manipulation
+		# Iterator boolean --> Tells programw when to terminate
+		done = False 
+		# Very important bool for mouse manipulation
+		dummy = False 
 		self.buildMenu()
 
 		while screenloop:
 			self.clock.tick(30)
-			(depth,_) = get_depth() #Get the depth from the kinect 
+			# Get the depth from the kinect
+			(depth,_) = get_depth()  
 			old_depth = depth
 			depth = cv2.resize(old_depth, (1024, 768))
-			depth = depth.astype(np.float32) #Convert the depth to a 32 bit float
-			_,depthThresh = cv2.threshold(depth, 600, 255, cv2.THRESH_BINARY_INV) #Threshold the depth for a binary image. Thresholded at 600 arbitary units
-			_,back = cv2.threshold(depth, 900, 255, cv2.THRESH_BINARY_INV) #Threshold the background in order to have an outlined background and segmented foreground
-			blobData = BlobAnalysis(depthThresh) #Creates blobData object using BlobAnalysis class
-			blobDataBack = BlobAnalysis(back) #Creates blobDataBack object using BlobAnalysis class
+			# Convert the depth to a 32 bit float
+			depth = depth.astype(np.float32)
+			# Threshold the depth for a binary image. Thresholded at 600 arbitary units
+			_,depthThresh = cv2.threshold(depth, 600, 255, cv2.THRESH_BINARY_INV) 
+			# Threshold the background in order to have an outlined background and segmented foreground
+			_,back = cv2.threshold(depth, 900, 255, cv2.THRESH_BINARY_INV) 
+			# Creates blobData object using BlobAnalysis class
+			blobData = BlobAnalysis(depthThresh) 
+			# Creates blobDataBack object using BlobAnalysis class
+			blobDataBack = BlobAnalysis(back) 
 			
 			mpos = pygame.mouse.get_pos() 
 
@@ -162,51 +174,73 @@ class IdleScreen():
 			else:
 				self.activeFocus = 0
 				self.lastActiveFocus = 1
-		
-			del depth #Deletes depth --> opencv memory issue
-			screenFlipped = pygame.transform.flip(screen,1,0) #Flips the screen so that it is a mirror display
-			screen.blit(screenFlipped,(0,0)) #Updates the main screen --> screen
-			pygame.display.flip() #Updates everything on the window
 			
-			#Mouse Try statement
+			# Deletes depth --> opencv memory issue
+			del depth 
+			# Flips the screen so that it is a mirror display
+			screenFlipped = pygame.transform.flip(screen,1,0) 
+			# Updates the main screen --> screen
+			screen.blit(screenFlipped,(0,0))
+			# Updates everything on the window
+			pygame.display.flip() 
+			
+			# Mouse Try statement
 			try:
 				centroidX = blobData.centroid[0][0]
 				centroidY = blobData.centroid[0][1]
 				if dummy:
-					mousePtr = display.Display().screen().root.query_pointer()._data #Gets current mouse attributes
-					dX = centroidX - strX #Finds the change in X
-					dY = strY - centroidY #Finds the change in Y
+					# Gets current mouse attributes
+					mousePtr = display.Display().screen().root.query_pointer()._data 
+					# Finds the change in X
+					dX = centroidX - strX 
+					# Finds the change in Y
+					dY = strY - centroidY 
 					print mouseX, mouseY
 					minChange = 2
-					if abs(dX) > minChange: #If there was a change in X greater than 1...
-						mouseX = mousePtr["root_x"] - 2*dX #New X coordinate of mouse
+					# If there was a change in X greater than minChange...
+					if abs(dX) > minChange:
+						# New X coordinate of mouse
+						mouseX = mousePtr["root_x"] - 2*dX 
 						if mouseX < 0:
 							mouseX = 0
 						elif mouseX > self.scrWidth:
 							mouseX = self.scrWidth
-					if abs(dY) > minChange: #If there was a change in Y greater than 1...
-						mouseY = mousePtr["root_y"] - 2*dY #New Y coordinate of mouse
+					# If there was a change in Y greater than 1...
+					if abs(dY) > minChange: 
+						# New Y coordinate of mouse
+						mouseY = mousePtr["root_y"] - 2*dY 
 						if mouseY < 0:
 							mouseY = 0
 						elif mouseY > self.scrHeight:
 							mouseY = self.scrHeight
 					
-					move_mouse(mouseX,mouseY) #Moves mouse to new location
-					strX = centroidX #Makes the new starting X of mouse to current X of newest centroid
-					strY = centroidY #Makes the new starting Y of mouse to current Y of newest centroid
-					cArea = cacheAppendMean(cHullAreaCache,blobData.cHullArea[0]) #Normalizes (gets rid of noise) in the convex hull area
-					areaRatio = cacheAppendMean(areaRatioCache, blobData.contourArea[0]/cArea) #Normalizes the ratio between the contour area and convex hull area
+					# Moves mouse to new location
+					move_mouse(mouseX, mouseY)
+					# Makes the new starting X of mouse to current X of newest centroid 
+					strX = centroidX 
+					# Makes the new starting Y of mouse to current Y of newest centroid
+					strY = centroidY 
+					# Normalizes (gets rid of noise) in the convex hull area
+					cArea = cacheAppendMean(cHullAreaCache,blobData.cHullArea[0]) 
+					# Normalizes the ratio between the contour area and convex hull area
+					areaRatio = cacheAppendMean(areaRatioCache, blobData.contourArea[0]/cArea)
 					print cArea, areaRatio, "(Must be: < 1000, > 0.82)"
-					if cArea < 25000 and areaRatio > 0.82: #Defines what a click down is. Area must be small and the hand must look like a binary circle (nearly)
+					# Defines what a click down is. Area must be small and the hand must look like a binary circle (nearly)
+					if cArea < 25000 and areaRatio > 0.82: 
 						click_down(1)
 					else:
 						click_up(1)
 				else:
-					strX = centroidX #Initializes the starting X
-					strY = centroidY #Initializes the starting Y
-					dummy = True #Lets the function continue to the first part of the if statement
-			except: #There may be no centroids and therefore blobData.centroid[0] will be out of range
-				dummy = False #Waits for a new starting point
+					# Initializes the starting X
+					strX = centroidX 
+					# Initializes the starting Y
+					strY = centroidY
+					# Lets the function continue to the first part of the if statement 
+					dummy = True 
+			except: 
+				# There may be no centroids and therefore blobData.centroid[0] will be out of range
+				# Waits for a new starting point
+				dummy = False 
 
 	# Handles, creates and updates floating/bouncing animals in menu screen
 	def floatingPicture(self):
@@ -228,9 +262,11 @@ This class is a less extensive form of regionprops() developed by MATLAB.
 It finds properties of contours and sets them to fields
 """
 class BlobAnalysis:
-	def __init__(self,BW): #Constructor. BW is a binary image in the form of a numpy array
+	# Constructor. BW is a binary image in the form of a numpy array
+	def __init__(self,BW): 
 		self.BW = BW
-		cs = cv.FindContours(cv.fromarray(self.BW.astype(np.uint8)),cv.CreateMemStorage(),mode = cv.CV_RETR_EXTERNAL) #Finds the contours
+		# Finds the contours
+		cs = cv.FindContours(cv.fromarray(self.BW.astype(np.uint8)),cv.CreateMemStorage(),mode = cv.CV_RETR_EXTERNAL) 
 		counter = 0
 		"""
 		These are dynamic lists used to store variables
@@ -240,23 +276,37 @@ class BlobAnalysis:
 		contours = list()
 		cHullArea = list()
 		contourArea = list()
-		while cs: #Iterate through the CvSeq, cs.
-			if abs(cv.ContourArea(cs)) > 5000: #Filters out contours smaller than 2000 pixels in area
-				contourArea.append(cv.ContourArea(cs)) #Appends contourArea with newest contour area
-				m = cv.Moments(cs) #Finds all of the moments of the filtered contour
+		# Iterate through the CvSeq, cs.
+		while cs: 
+			# Filters out contours smaller than 2500 pixels in area
+			if abs(cv.ContourArea(cs)) > 2500: 
+				# Appends contourArea with newest contour area
+				contourArea.append(cv.ContourArea(cs)) 
+				# Finds all of the moments of the filtered contour
+				m = cv.Moments(cs) 
 				try:
-					m10 = int(cv.GetSpatialMoment(m,1,0)) #Spatial moment m10
-					m00 = int(cv.GetSpatialMoment(m,0,0)) #Spatial moment m00
-					m01 = int(cv.GetSpatialMoment(m,0,1)) #Spatial moment m01
-					centroid.append((int(m10/m00), int(m01/m00))) #Appends centroid list with newest coordinates of centroid of contour
-					convexHull = cv.ConvexHull2(cs,cv.CreateMemStorage(),return_points=True) #Finds the convex hull of cs in type CvSeq
-					cHullArea.append(cv.ContourArea(convexHull)) #Adds the area of the convex hull to cHullArea list
-					cHull.append(list(convexHull)) #Adds the list form of the convex hull to cHull list
-					contours.append(list(cs)) #Adds the list form of the contour to contours list
-					counter += 1 #Adds to the counter to see how many blobs are there
+					# Spatial moment m10
+					m10 = int(cv.GetSpatialMoment(m,1,0)) 
+					# Spatial moment m00
+					m00 = int(cv.GetSpatialMoment(m,0,0))
+					# Spatial moment m01 
+					m01 = int(cv.GetSpatialMoment(m,0,1))
+					# Appends centroid list with newest coordinates of centroid of contour
+					centroid.append((int(m10/m00), int(m01/m00))) 
+					# Finds the convex hull of cs in type CvSeq
+					convexHull = cv.ConvexHull2(cs,cv.CreateMemStorage(),return_points=True) 
+					# Adds the area of the convex hull to cHullArea list
+					cHullArea.append(cv.ContourArea(convexHull))
+					# Adds the list form of the convex hull to cHull list
+					cHull.append(list(convexHull))
+					# Adds the list form of the contour to contours list 
+					contours.append(list(cs)) 
+					# Adds to the counter to see how many blobs are there
+					counter += 1 
 				except:
 					pass
-			cs = cs.h_next() #Goes to next contour in cs CvSeq
+			# Goes to next contour in cs CvSeq
+			cs = cs.h_next() 
 		"""
 		Below the variables are made into fields for referencing later
 		"""
@@ -267,20 +317,25 @@ class BlobAnalysis:
 		self.cHullArea = cHullArea
 		self.contourArea = contourArea
 
-d = display.Display() #Display reference for Xlib manipulation
-def move_mouse(x,y):#Moves the mouse to (x,y). x and y are ints
+# Display reference for Xlib manipulation
+d = display.Display()
+
+# Moves the mouse to (x,y). x and y are ints
+def move_mouse(x,y):
 	print "Moving mouse to: ", x, y
 	s = d.screen()
 	root = s.root
 	root.warp_pointer(x,y)
 	d.sync()
 	
-def click_down(button):#Simulates a down click. Button is an int
+# Simulates a down click. Button is an int
+def click_down(button):
 	print "GOT CLICK DOWN"
 	Xlib.ext.xtest.fake_input(d,X.ButtonPress, button)
 	d.sync()
 	
-def click_up(button): #Simulates a up click. Button is an int
+# Simulates a up click. Button is an int
+def click_up(button):
 	print "GOT CLICK UP"
 	Xlib.ext.xtest.fake_input(d,X.ButtonRelease, button)
 	d.sync()
